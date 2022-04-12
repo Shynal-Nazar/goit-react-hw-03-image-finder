6f039ebea3aeeb823d6b9c0ca0be574bc03d1fc3
Shynal Nazar <Pro100nazar@i.ua>
2022-04-12 16:27:40 +0300

    delete wrong kod

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -8,29 +8,6 @@ const override = css`
   border-color: red;
 `;
 
-// export default class Loader extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       loading: true,
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <div className="sweet-loading">
-//         <ClipLoader
-//           css={override}
-//           size={150}
-//           color={'#123abc'}
-//           loading={this.state.loading}
-//           speedMultiplier={1.5}
-//         />
-//       </div>
-//     );
-//   }
-// }
-
 const Loader = () => {
   return (
     <div className="sweet-loading">
